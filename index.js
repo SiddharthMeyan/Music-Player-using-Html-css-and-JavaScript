852200e3ea388a4548987f8b55365ea762c94b00
SiddharthMeyan <siddharthmeyan@gmail.com>
2021-05-12 19:38:41 +0530

    random music, shuffle,random cover photos

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+music_list=['/music/starboy.mp3','/music/good_song.mp3','music/old_lofi.mp3']
 songname = document.getElementById('songname')
-songname.innerHTML = "Lovely"
+imgg = document.getElementById("imgg")
+
+const shuffle =()=>{
+    randomInt=Math.ceil(Math.random() * (music_list.length ) + (-1));
+    rand=music_list[randomInt]
+    console.log(rand);
+    const audio = new Audio(rand)
+    audio.play()
+}
+const shuffleImg =()=>{
+    randomInt=Math.ceil(Math.random() * (music_list.length ) + (-1));
+    
+}
+
+imgg.src='https://source.unsplash.com/300x300/?lofi'
+
+const audio = new Audio('/music/starboy.mp3')
+
+playMusic = ()=>{
+    audio.play()
+}
+
+
+pauseMusic = ()=>{
+    audio.pause()
+}
+
