9dc7941a3c689e235e0e49f647a6c62480a47b21
SiddharthMeyan <siddharthmeyan@gmail.com>
2021-05-13 12:44:20 +0530

    stop, pause and button change on play pause

--- index.js
+++ index.js
@@ -1,29 +1,77 @@
-music_list=['/music/starboy.mp3','/music/good_song.mp3','music/old_lofi.mp3']
-songname = document.getElementById('songname')
-imgg = document.getElementById("imgg")
+music_list = [
+  "/music/starboy.mp3",
+  "/music/good_song.mp3",
+  "/music/old_lofi.mp3",
+];
+songname = document.getElementById("songname");
+imgg = document.getElementById("imgg");
+playorpause = document.getElementById("playorpause");
 
-const shuffle =()=>{
-    randomInt=Math.ceil(Math.random() * (music_list.length ) + (-1));
-    rand=music_list[randomInt]
-    console.log(rand);
-    const audio = new Audio(rand)
-    audio.play()
-}
-const shuffleImg =()=>{
-    randomInt=Math.ceil(Math.random() * (music_list.length ) + (-1));
-    
-}
+let musicShuffle = false;
 
-imgg.src='https://source.unsplash.com/300x300/?lofi'
+const shuffle = () => {
+  randomInt = Math.ceil(Math.random() * music_list.length + -1);
+  rand = music_list[randomInt];
+  const audio = new Audio(rand);
+  audio.play();
+  musicShuffle = true;
+  myaud(audio);
+};
+const shuffleImg = () => {
+  randomInt = Math.ceil(Math.random() * music_list.length + -1);
+};
 
-const audio = new Audio('/music/starboy.mp3')
+const myaud = (audio) => {
+  maudio = audio;
+  return maudio;
+};
 
-playMusic = ()=>{
-    audio.play()
-}
+imgg.src = "https://source.unsplash.com/300x300/?lofi";
 
+// const audio = new Audio('/music/starboy.mp3')
 
-pauseMusic = ()=>{
-    audio.pause()
-}
+playMusic = (myfiledes) => {
+  amIcliked = document.getElementById("play");
+  amIcliked.addEventListener("click", () => (musicShuffle = false));
+  audio = new Audio(myfiledes);
+  audio.play();
+  pauseBtn();
+};
+maudio = myaud();
+stopMusic = () => {
+  if (musicShuffle) {
+    audio = maudio;
+  }
+  audio.pause();
+  audio.currentTime = 0;
+};
 
+pauseMusic = () => {
+  if (musicShuffle) {
+    audio = maudio;
+  }
+  audio.pause();
+  playBtn();
+};
+
+pauseBtn = () => {
+  if (!audio.paused) {
+    playorpause.innerHTML = `<i
+      id="pause"
+      onclick="pauseMusic()"
+      class="fa fa-pause"
+      style="font-size: 36px; margin-left: 20px"
+      aria-hidden="true"
+    ></i>`;
+  }
+};
+playBtn = () => {
+  playorpause.innerHTML = `<i
+    id="play"
+    onclick="playMusic('/music/starboy.mp3')"
+    class="fa fa-play"
+    style="font-size: 36px; margin-left: 20px"
+    aria-hidden="true"
+  ></i
+  >`;
+};
